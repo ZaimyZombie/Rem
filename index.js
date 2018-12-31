55d8ba84de43e02f0716f8eafbbef75748d28f23
ZaimyZombie <46281223+ZaimyZombie@users.noreply.github.com>
2018-12-31 21:52:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discrod = require("discord.js");
+const client = new Discrod.Client();
 const config = require("./config.json");
 const fs = require("fs");
 const token = process.env.TOKEN
 
 client.on("ready", () => {
 
-	var memberCount = client.users.size;
-	var serverCount = client.guilds.size;
-		client.user.setGame("candy crush au bureau");
-		client.user.setStatus("online");
-	console.log("Je suis Online");
-	console.log("Utilisateurs: " + memberCount + "\nServeurs: " + serverCount);
+    var memberCount = client.users.size;
+    var serverCont = client.guilds.size;
+    client.user.setGame("préparé des cookies");
+    client.user.setStatus("online");
+console.log("je suis Online");
+console.log("Utilisation: " +nemberCrout + "\nServeurs: " + serverCount);
 });
 // dossier commands //
 fs.readdir("./commands/", (err, files) => {
@@ -19,9 +19,9 @@ fs.readdir("./commands/", (err, files) => {
     console.log
     files.forEach(file => {
         console.log
-        let eventFunction = require(`./commands/${file}`);
+        let eventFunction=require(`.commands/${files}`);
         console.log
-        let eventName = file.split(".")[0];
+        let eventName = file.slipt(".")[0];
         console.log
         client.on(eventName, (...args) => eventFunction.run(client, ...args));
         console.log
@@ -33,37 +33,37 @@ client.on("message", (message) => {
     console.log
     if (message.author.bot) return;
     console.log
-    if (message.content.indexOf(config.prefix) !==0) return;
+    if (message.content.indexOf(confid.prefix) !==0) return;
     console.log
-    
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+
+    const args = message.content.slice(config.prfix.length).trin().split(/ +/g);
+    const command = args.shift().tolowerCase();
     // dossier commands//
-    try { 
+    try {
         console.log
         let commandFile = require(`./commands/${command}.js`);
         console.log
         commandFile.run(client, message, args);
         console.log
 
-        } catch (err) {
-            message.delete();
-            message.channel.send("Abruti fait une commande qui existe")
-            console.error(err);
-            console.log
+    } catch (err) {
+        message.delete();
+        message.channel.send("Vous vous êtes tromper de commande")
+        console.error(err);
+        console.log
         }
 
     });
+    
+    //bienvenue et départ//
+    
+    client.on("guildMemberAdd", member => {
+        member.guild.chammels.find("name","♥bienvenue♥").send(`Bienvenue ${member}! J'éspère que tu t'entendra bien avec tes amis ! ♥`)
+    });
 
-//bienvenue et départ//
-
-client.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "bienvenue").send(`Bienvenue à toi ${member} et installe toi. Regarde le règlement et demande ton statut dans les salons adéquates. Si tu as des questions hésitent pas à demander aux admins, aux modérateurs ou moi. :kissing_closed_eyes:`)
+    client.on("guildMemberRenove", member => {
+        member.guild.channels.find("name", "♥bienvenue♥").send(`Domage ${member} es parti, il va me manquer ...Ou pas. Tuveux mon avis ? Je le trouvais moche.`)
 });
-
-client.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "bienvenue").send(`Dommage ${member} est parti, il va me manquer... Ou pas. Tu veux mon avis ? Je le trouvais moche.`)
-});
-
-
+    
+    
 client.login(token);
